035f030121c42789ffc0a1a463ebb5e433dad2b8
nimamovic <nimamovic3@etf.unsa.ba>
2019-03-20 13:13:43 +0100

    initial commite

---
+++ rgwef/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rgwef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
